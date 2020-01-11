0f2a2ed6b92894fee4e8becef695fb62dff6bdeb
Marcin Szewczyk <marcin91.szewczyk@gmail.com>
2020-01-11 12:46:25 +0100

    Dodaj klasę Connection

---
+++ src/main/java/eu/marcinszewczyk/singleton/MainAppSingleton.java
@@ -0,0 +1,11 @@
+package eu.marcinszewczyk.singleton;
+
+public class MainAppSingleton {
+
+    public static void main(String[] args) {
+        Connection pierwszy = new Connection();
+        Connection drugi = new Connection();
+        Connection trzeci = new Connection();
+    }
+
+}
